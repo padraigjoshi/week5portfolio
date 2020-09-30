5a4ca08d8a6bd4457b507625ecaeed822febdaf3
patrick joshi <pjoshi4@gmu.edu>
2020-09-30 13:35:23 -0400

    week 5

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Drama' },
+  { id: 2, name: 'Comedy' },
+  { id: 3, name: 'Action' },
+  { id: 4, name: 'Horror' },
+  { id: 5, name: 'Documentary' },
+];
